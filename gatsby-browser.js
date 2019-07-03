4fa2e0fa89b92e297eea12cfb00423cbc3c9d6f2
David Cabrera <david@rerainc.com>
2019-07-03 17:47:12 -0400

    fix site browser resolver

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import './src/browser.js'
 
 try {
-  require(`../../site/browser.js`)
+  require(`./site/browser.js`)
 } catch (e) {
   if (e.code === 'MODULE_NOT_FOUND') {
     console.log('Site browser scripts not found.')
